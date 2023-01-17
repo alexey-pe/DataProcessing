60371df78e5d6ab7009a5938ae6dedc62c979ccc
oleksiy.perro <oleksiy.perro@getinge.com>
2023-01-17 08:28:36 +0200

    BodyMassCalculations:  initial commit.

---
+++ BodyMassCalculations/BodyMassCalculations.py
@@ -0,0 +1,41 @@
+﻿
+data = open("D:\planning\operations\Масса_тела.txt", "r")
+total = 0.0
+count = 0
+shitTotal = 0.0
+weightNumbers = []
+for line in data.readlines():
+    lastSpaceIndex = line.rfind(" ")
+    if lastSpaceIndex < 0:
+        continue
+
+    weightStr = line[lastSpaceIndex + 1:]
+
+    if weightStr[0] == "(" and weightStr[-2] == ")":
+        withShitStr = weightStr[1:-2]
+        beforeLastSpaceIndex = line.rfind(" ", 0, lastSpaceIndex)
+        shitlessStr = line[beforeLastSpaceIndex + 1:lastSpaceIndex]
+        withShitNumber = float(withShitStr)
+        shitlessNumber = float(shitlessStr)
+        shitNumber = withShitNumber - shitlessNumber
+        shitTotal += shitNumber
+        print(weightStr[:-1], withShitStr, beforeLastSpaceIndex, shitlessStr, round(shitNumber, 2))
+        continue
+
+    if weightStr[:-1].isalpha():
+        continue
+
+    weightNumber = float(weightStr)
+    weightNumbers.append(weightNumber)
+    total = total + weightNumber
+    count += 1
+    # print(weightNumber, count, total)
+
+print(f"Average equals {round(total / count, 2)}")
+
+print(f"Total shit equals {round(shitTotal, 2)}")
+
+# print(weightNumbers)
+
+print(max(weightNumbers))
+print(min(weightNumbers))
